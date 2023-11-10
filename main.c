894f9517b7807f887f6809699c5a2f3754921a0b
princz.1 <150322497+princz-1@users.noreply.github.com>
2023-11-10 17:24:05 +0530

    Function & Recursion Basic Code
    Basic Code

--- main.c
+++ main.c
@@ -1,3 +1,20 @@
+//Function Basic code 
+#include <stdio.h>
+void printhw(); // Functions declaration / prototype
+void goodbye();
+int main(void) {
+
+  printhw(); // Functions calling
+  goodbye();
+}
+void printhw() { // Functions defination
+  printf("Hello World\n");
+  printf("my name is prince panchal\n");
+}
+void goodbye() {
+  printf("Good bye"); 
+}
+
 // Basic for Recursion
 
 #include <stdio.h>
