b7f8df395487120c2fac9a4a09986f2f51c352c1
princz.1 <150322497+princz-1@users.noreply.github.com>
2023-11-10 17:06:50 +0530

    Function&Recursion Basic Code
    Basic Code

--- main.c
+++ main.c
@@ -14,3 +14,20 @@ void printhw() { // Functions defination
 void goodbye() {
   printf("Good bye"); 
 }
+
+
+// Basic for Recursion
+#include <stdio.h>
+void printHW(int count);
+int main(void) {
+  printHW(5);
+  return 0;
+}
+//Recursive Functions
+void printHW(int count){
+  if(count ==0){
+    return;
+  }
+  printf("Hello World\n");
+  printHW(count-1);
+}
