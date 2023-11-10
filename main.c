aed6c7ba4d0dc91b96cb82e698f153c088d99f7a
princz-1 <150322497+princz-1@users.noreply.github.com>
2023-11-10 07:40:07 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void printhw(); // Functions declaration / prototype
+void goodbye();
+int main(void) {
+
+  printhw(); // Functions calling
+  goodbye();
+}
+void printhw() { // Functions defination
+  printf("Hello World\n");
+  printf("my name is prince panchal\n");
+}
+void goodbye() {
+  printf("Good bye"); 
+}
