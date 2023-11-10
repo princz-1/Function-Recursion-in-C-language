c73213330866f772024adae9888de890cf7c0d08
princz.1 <150322497+princz-1@users.noreply.github.com>
2023-11-10 13:22:11 +0530

    Question.1
    //Write a function that prints Namaste if user is indian & Bonjour if the user is French ?

---
+++ Que1.c
@@ -0,0 +1,24 @@
+//Write a function that prints Namaste if user is indian & Bonjour if the user is French ?
+
+#include <stdio.h>
+void namaste();
+void bonjour();
+int main(void) {
+  char ch;
+  printf("Enter i for indian and f for french: ");
+  scanf( "%c", &ch);
+  if(ch == 'i'){
+    namaste();
+  }
+  else{
+    bonjour();
+  }
+  return 0;
+}
+
+void namaste(){
+  printf("Namaste");
+}
+void bonjour(){
+  printf("Bonjour");
+}
